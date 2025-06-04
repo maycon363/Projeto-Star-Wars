f13c04aa812bb886fb41f939eca4ae0f821b0135
Maycon Borges <mayconborgesgato717@gmail.com>
2025-06-03 22:02:03 -0300

    Melhorias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/Projeto-Star-Wars/', // 👈 Isso aqui é o nome do repositório
   plugins: [react()],
-})
+});
